8eb3de0c8f2f3fe0defa2805e084db43255f904d
Rohit Jones <rohitjones21@gmail.com>
2016-09-10 16:11:52 +0530

    Astar Done !!!!!

--- AI.py
+++ AI.py
@@ -1,15 +1,108 @@
 from main import *
-game_QUIT = False
+from copy import deepcopy
+from random import randint
+from time import sleep
+
+Previous_States = []
 
 def heuristic_value(input_board):
-    value = 0
-    Board.index(1) #should be 0
+    coordinates = {0: (0, 0), 1: (1, 0), 2: (2, 0),
+                   3: (0, 1), 4: (1, 1), 5: (2, 1),
+                   6: (0, 2), 7: (1, 2), 8: (2, 2)}
 
+    def distance(n, m):
+        x1, y1 = coordinates[n]
+        x2, y2 = coordinates[m]
+        return abs(x1 - x2) + abs(y1 - y2)
+
+    value = 0
+    for x in range(9):
+        value += distance(input_board.index(x), x)
+
+    return value
+
+
+def astart(current_board=Board):
+    previous_move = None
+    while not is_solved(current_board):
+        possible_states = []
+        temp_list = possible_moves(current_board, previous_move)
+        for each in temp_list:
+            temp = iMove(each, current_board)
+            possible_states.append((heuristic_value(temp), each, temp))
+
+        possible_states.sort(key=lambda val: val[0])
+        previous_move = possible_states[0][1]
+        move(possible_states[0][1], current_board)
+        display_refresh(current_board)
+        sleep(0.030)
+
+def hillclimbing(current_board=Board):
+    if current_board in Previous_States:
+        return
+    else:
+        Previous_States.append(current_board)
+
+    while not is_solved(current_board):
+
+        current_h_value = heuristic_value(current_board)
+        possible_states = []
+        temp_list = possible_moves(current_board)
+        for each in temp_list:
+            temp = iMove(each, current_board)
+            possible_states.append((heuristic_value(temp), each, temp))
+
+        possible_states.sort(key=lambda val: val[0])
+
+        if possible_states[0][0] < current_h_value:
+            move(possible_states[0][1], current_board)
+
+        elif possible_states[0][0] == current_h_value:
+            for each in possible_states:
+                if each[0] == current_h_value:
+                    hillclimbing(each[2])
+
+        else:
+            count = randint(5, 10)
+            for x in range(count):
+                init_game_temp = possible_moves(current_board)
+                move(init_game_temp[randint(0, len(init_game_temp) - 1)], current_board)
+
+        display_refresh(current_board)
+
+    print(current_board)
+
+def iMove(position, current_board=Board):
+    def swap(pos_1, pos_2):
+        local_board[pos_1], local_board[pos_2] = local_board[pos_2], local_board[pos_1]
+
+    local_board = deepcopy(current_board)
+    position = position.lower()
+    current_pos = local_board.index(8)
+
+    if position == 'down':
+        swap(current_pos, (current_pos - 3))
+
+    if position == 'up':
+        swap(current_pos, (current_pos + 3))
+
+    if position == 'right':
+        swap(current_pos, (current_pos - 1))
+
+    if position == 'left':
+        swap(current_pos, (current_pos + 1))
+
+    return local_board
 
 if __name__ == "__main__":
-    init_game()
+    init_game(10)
     display_refresh()
-
-    while not is_solved() and not game_QUIT:
-        keyboard_input()
-        display_refresh()
+    # hillclimbing()
+    # print(Board)
+    # input("start !")
+    astart()
+    # input("solved !")
+    # print(Board)
+    # temp = iMove('up')
+    # print(temp)
+    # print(Board)
--- main.py
+++ main.py
@@ -2,32 +2,30 @@ import pygame
 from random import randint
 from sys import exit
 
-Board = [0 for useless_variable in range(9)]
+Board = [iterator_x for iterator_x in range(9)]
 Moves = ['down', 'up', 'right', 'left']
 Tiles = []
 game_QUIT = False
-hints_enabled = False
+hints_enabled = True
 
 pygame.init()
 resolution = 620, 620
 screen = pygame.display.set_mode(resolution)
 
-positions = {
-    0: (5, 5, 200, 200),
-    1: (210, 5, 200, 200),
-    2: (415, 5, 200, 200),
-
-    3: (5, 210, 200, 200),
-    4: (210, 210, 200, 200),
-    5: (415, 210, 200, 200),
-
-    6: (5, 415, 200, 200),
-    7: (210, 415, 200, 200),
-    8: (415, 415, 200, 200)
-}
-
-
 class Tile:
+    positions = {
+        0: (5, 5, 200, 200),
+        1: (210, 5, 200, 200),
+        2: (415, 5, 200, 200),
+
+        3: (5, 210, 200, 200),
+        4: (210, 210, 200, 200),
+        5: (415, 210, 200, 200),
+
+        6: (5, 415, 200, 200),
+        7: (210, 415, 200, 200),
+        8: (415, 415, 200, 200)
+    }
     GREEN = 0, 255, 0
     RED = 255, 0, 0
     BLACK = 0, 0, 0
@@ -35,8 +33,8 @@ class Tile:
     def __init__(self, value):
         self.value = value
 
-    def render_tile(self, output=screen):
-        b_gfx_p = positions[Board.index(self.value)]  # board_gfx_position
+    def render_tile(self, output=screen, current_board=Board):
+        b_gfx_p = self.positions[current_board.index(self.value)]  # board_gfx_position
         color = self.RED
         if position_is_correct(self.value):
             color = self.GREEN
@@ -46,36 +44,36 @@ class Tile:
         if hints_enabled:
             text_size = 217
             font = pygame.font.Font(None, text_size)
-            ren = font.render(str(self.value), 0, color, self.BLACK)
+            ren = font.render(str(self.value + 1), 0, color, self.BLACK)
             pygame.draw.rect(output, self.BLACK, pygame.Rect(b_gfx_p[0] + 25, b_gfx_p[1] + 25, 150, 150))
             screen.blit(ren, (b_gfx_p[0] + 34 + 25, b_gfx_p[1] + 30))
             pygame.draw.rect(output, color, pygame.Rect(b_gfx_p[0], b_gfx_p[1] + 175, 200, 20))
-
-def init_game():
+def init_game(value=10):
     global Tiles
-    Tiles = [Tile(x + 1) for x in range(8)]
-    print("inspection point !")
-    for make_game_x in range(8):
-        Board[make_game_x] = (make_game_x + 1)
-
-    for x in range(10):
-        init_game_temp = possible_moves()
-        move(init_game_temp[randint(0, len(init_game_temp) - 1)])
-def position_is_correct(value):
-    if value == 0 and Board[9] == 0:
-        return True
-
-    elif Board[value - 1] == value:
+    global Board
+    Tiles = [Tile(x) for x in range(8)]
+    for x in range(value):
+        init_game_temp = possible_moves(Board)
+        Board = move(init_game_temp[randint(0, len(init_game_temp) - 1)], Board)
+def position_is_correct(value, current_board=Board):
+    if current_board[value] == value:
         return True
 
     return False
-def is_solved():
-    for checker_x in range(8):
-        if Board[checker_x] != (checker_x + 1):
+def is_solved(current_board=Board):
+    for checker_x in range(9):
+        if current_board[checker_x] != checker_x:
             return False
 
     return True
-def possible_moves():
+def possible_moves(board_to_check=Board, prev_move=None):
+    opposite = {
+        'up': 'down',
+        'down': 'up',
+        'left': 'right',
+        'right': 'left'
+    }
+
     possible_moves_temp = [True, True, True, True]
 
     upper_edge = [0, 1, 2]
@@ -83,7 +81,7 @@ def possible_moves():
     lower_edge = [6, 7, 8]
     right_edge = [2, 5, 8]
 
-    pos = Board.index(0)
+    pos = board_to_check.index(8)
 
     if pos in upper_edge:
         possible_moves_temp[0] = False
@@ -94,14 +92,19 @@ def possible_moves():
     if pos in right_edge:
         possible_moves_temp[3] = False
 
-    return [Moves[x] for x in range(4) if possible_moves_temp[x] is True]
-def move(position):
+    temp = [Moves[x] for x in range(4) if possible_moves_temp[x] is True]
+    if prev_move is not None:
+        temp.remove(opposite[prev_move])
+
+    return temp
+
+def move(position, current_board=Board):
     def swap(pos_1, pos_2):
-        global Board
-        Board[pos_1], Board[pos_2] = Board[pos_2], Board[pos_1]
+        # global Board
+        current_board[pos_1], current_board[pos_2] = current_board[pos_2], current_board[pos_1]
 
     position = position.lower()
-    current_pos = Board.index(0)
+    current_pos = current_board.index(8)
 
     if position == 'down':
         swap(current_pos, (current_pos - 3))
@@ -115,27 +118,30 @@ def move(position):
     if position == 'left':
         swap(current_pos, (current_pos + 1))
 
-def display_refresh():
+    return current_board
+
+def display_refresh(current_board=Board):
     screen.fill((0, 0, 0))
     for each in Tiles:
-        each.render_tile()
+        each.render_tile(current_board=current_board)
     pygame.display.flip()
     pygame.time.Clock().tick(60)
 def keyboard_input():
     global hints_enabled
+    global Board
     for event in pygame.event.get():
-        if is_solved():
+        if is_solved(Board):
             break
 
         if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_UP and "up" in possible_moves():
-                move("up")
-            if event.key == pygame.K_DOWN and "down" in possible_moves():
-                move("down")
-            if event.key == pygame.K_LEFT and "left" in possible_moves():
-                move("left")
-            if event.key == pygame.K_RIGHT and "right" in possible_moves():
-                move("right")
+            if event.key == pygame.K_UP and "up" in possible_moves(Board):
+                Board = move("up")
+            if event.key == pygame.K_DOWN and "down" in possible_moves(Board):
+                Board = move("down")
+            if event.key == pygame.K_LEFT and "left" in possible_moves(Board):
+                Board = move("left")
+            if event.key == pygame.K_RIGHT and "right" in possible_moves(Board):
+                Board = move("right")
 
             if event.key == pygame.K_RSHIFT or event.key == pygame.K_CAPSLOCK:
                 hints_enabled = True
@@ -151,6 +157,6 @@ def keyboard_input():
 if __name__ == "__main__":
     init_game()
     display_refresh()
-    while not is_solved():
+    while not is_solved(Board):
         keyboard_input()
         display_refresh()
